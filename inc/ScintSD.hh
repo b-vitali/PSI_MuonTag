2055a294e4759ae125972b6f5948fa5c2aaa03b7
b-vitali <bastiano.vitali@gmail.com>
2022-03-25 10:52:42 +0100

    Column_ID introduced

--- inc/ScintSD.hh
+++ inc/ScintSD.hh
@@ -40,37 +40,45 @@ class ScintSD : public G4VSensitiveDetector{
 	private:
 		ScintHitsCollection* fScintCollection;
 		G4int fEvent, fScintNo, fParticleID;
+		G4int fEvent_ID, fScintNo_ID, fParticleID_ID;
+
 		G4double fEin, fEdep, fEout, fDelta, fThetaIn, fTrackLength, fThetaOut;
+		G4double fEin_ID, fEdep_ID, fEout_ID, fDelta_ID, fThetaIn_ID, fTrackLength_ID, fThetaOut_ID;
+
+		G4int fRight, fLeft, fDown, fUp, fBack, fFront;
+		G4int fRight_ID, fLeft_ID, fDown_ID, fUp_ID, fBack_ID, fFront_ID;
+
 		G4int fBounce;
+
 		G4ThreeVector fDirIn, fDirOut;
 		G4ThreeVector fDirIn_trans, fDirOut_trans;
+		
 		G4ThreeVector fMomIn, fMomOut;
+		G4int fMomInX_ID, fMomInY_ID, fMomInZ_ID, fMomOutX_ID, fMomOutY_ID, fMomOutZ_ID;
 
-		std::vector<G4double> fTest;
+		G4ThreeVector fPosIn, fPosOut;
+		G4int fPosInX_ID, fPosInY_ID, fPosInZ_ID, fPosOutX_ID, fPosOutY_ID, fPosOutZ_ID;
+		G4double fTimeIn, fTimeOut;
+		G4int fTimeIn_ID, fTimeOut_ID;
 
-		G4ThreeVector fPosIn;
-		G4double fTimeIn;
-		G4ThreeVector fPosOut;
-		G4double fTimeOut;
+		G4int fNgamma, fNgammaSec, fNCer;
+		G4int fNgamma_ID, fNgammaSec_ID, fNCer_ID;
 
-		G4int fNgamma;
-		G4int fNgammaSec;
+		G4double fDecayTime;
+		G4int fDecayTime_ID;
+
 		std::vector<G4int> fCer;
 		std::vector<G4double> fThetaGamma;
 		std::vector<G4double> fTimeGamma;
 		std::vector<G4double> fEGamma;
-		G4int fNCer;
-		G4int fRight;
-		G4int fLeft;
-		G4int fDown;
-		G4int fUp;
-		G4int fBack;
-		G4int fFront;
 		G4int fSiPM;
-		G4double fDecayTime;
 		
 		G4int fPhotonsCmd, fTracksCmd;
 		G4int hitsCID;
+
+		std::vector<G4double> fTest;
+
+
 };
 
 #endif
